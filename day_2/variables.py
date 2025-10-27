e7af70a32b88a69c38934226990ceb6845c815ba
Hari5616 <kvumaramesh78@gmail.com>
2025-10-27 10:27:37 +0530

    Update variables.py

---
+++ day_2/variables.py
@@ -0,0 +1,47 @@
+#Day 2:30 Days of Python Programming
+
+first_name="Hariprasad"
+last_name="Ramesh"
+full_name="Hariprasad Ramesh"
+country="India"
+city="Vellore"
+age="17"
+year="2007"
+is_married=False
+is_true=True
+is_light_on=True
+my_skill=["HTML","C","PYTHON"]
+
+print(type(first_name))     
+print(type(last_name))
+print(type(full_name))  
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(my_skill))
+
+print(len(first_name))
+print(len(last_name))
+print(len(first_name)>len(last_name))
+num_one=5
+num_two=4
+total=num_one+num_two
+diff=num_one-num_two
+product=num_one*num_two 
+division=num_one/num_two
+reminder=num_one%num_two
+exp=num_one**num_two
+floor_division=num_one//num_two
+radius=30
+area_of_circle=3.14*(radius**2)
+circum_of_circle=2*3.14*radius
+radius=input("Enter the radius:")
+first_name=input("Enter your first name:")
+last_name=input("Enter your last name:")    
+country=input("Enter your country:")
+age=input("Enter your age:")
+#help("keywords")
